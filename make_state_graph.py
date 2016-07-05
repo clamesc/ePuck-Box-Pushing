e19e6b2d4101f1b654a2d6753349e8f1123ed203
Jan Rudolph <j.rudolph@tum.de>
2016-07-05 13:30:36 +0200

    Switch to doublecircle for final, calculate pos manually

--- make_state_graph.py
+++ make_state_graph.py
@@ -1,22 +1,33 @@
 import graphviz as gv
 import numpy as np
+import math
+
+
+def calc_circlepos(i, i_max):
+    radius = 3
+    posx = math.cos((math.pi * 2.0 * float(i) / float(i_max)) - 1.5 * math.pi) * radius
+    posy = math.sin((math.pi * 2.0 * float(i) / float(i_max)) - 1.5 * math.pi) * radius
+    return "{},{}!".format(posx, posy)
 
 if __name__ == "__main__":
-    T = np.load("T.npy")
+    T = np.load("Data/2016_07_05-2_T.npy")
     Pt = np.zeros((T.shape[0], T.shape[0]))
     for s in range(T.shape[0]):  # iterate over old states
         Pt[s] = np.zeros(T.shape[0])
-        for a in range(T.shape[1]):
+        for a in range(T.shape[1]):  # iterate over actions
             Pt[s] += T[s][a]
         Pt[s] = Pt[s] / np.sum(Pt[s])
     print Pt
 
-    dot = gv.Digraph(comment='The Round Table', graph_attr={('layout', 'circo')})
+    dot = gv.Digraph(comment='State Transition Probabilities',
+                     graph_attr={('layout', 'neato')},
+                     node_attr={('shape', 'circle')}
+                     )
     for i in range(0, T.shape[0]):
         if i == 0:
-            dot.node(str(i), label='desired direction')
+            dot.node(str(i), shape='doublecircle', pos=calc_circlepos(i, T.shape[0]))
         else:
-            dot.node(str(i))
+            dot.node(str(i), pos=calc_circlepos(i, T.shape[0]))
         for t in range(Pt.shape[1]):
             if Pt[i][t] != 0.0:
                 dot.edge(str(i), str(t), label=str("%0.2f" % Pt[i][t]))
